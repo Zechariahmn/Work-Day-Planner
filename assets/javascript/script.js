8c103d59159839f531fb1e22db19a587149153c4
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-11-04 12:28:40 -0500

    added if statement to check the times of the day and add classes for the backgorund indicators

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -20,3 +20,23 @@ $(document).ready(function () {
                     $(".time-block").each(function () {
                         var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
+                            //if statement to check the time and add the classes for background indicators
+                            if (blockTime < timeNow) {
+                                $(this).removeClass("future");
+                                $(this).removeClass("present");
+                                $(this).addClass("past");
+                            }
+                            else if (blockTime === timeNow) {
+                                $(this).removeClass("past");
+                                $(this).removeClass("future");
+                                $(this).addClass("present");
+                            }
+                            else {
+                                $(this).removeClass("present");
+                                $(this).removeClass("past");
+                                $(this).addClass("future");
+
+                            }
+                        })
+                    }
+
