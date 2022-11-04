5b190b941083bdda80a2e7ec44f57fa02fb65345
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-11-04 12:31:46 -0500

    added the ability to get the items from local storage

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -40,3 +40,20 @@ $(document).ready(function () {
                         })
                     }
 
+    // Get item from local storage if any
+    $("#hour7 .description").val(localStorage.getItem("hour7"));
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour18 .description").val(localStorage.getItem("hour18"));
+
+    timeTracker();
+})
+
