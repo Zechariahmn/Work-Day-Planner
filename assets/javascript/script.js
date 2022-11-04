767c7d9c6e1dce927490a5f4ef4167a86c38bac9
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-11-04 12:37:04 -0500

    added time block for 5AM

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,20 +5,20 @@ $("#currentDay").html(todayDate);
 // function that allows the users information and time entered to be stored into local storage
 $(document).ready(function () {
     $(".saveBtn").on("click", function () {
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+        let text = $(this).siblings(".description").val();
+        let time = $(this).parent().attr("id");
 
         // Save text in local storage
         localStorage.setItem(time, text);
     })
     
-            // function tracks the current time of the day and amount of hours
-                function timeTracker() {
-                    var timeNow = moment().hour();
+    function timeTracker() {
+        //Need to get the current hour (time count)
+        let timeNow = moment().hour();
 
                     // loop over time blocks
                     $(".time-block").each(function () {
-                        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+                        let blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
                             //if statement to check the time and add the classes for background indicators
                             if (blockTime < timeNow) {
