06c9e6063d6812359efd3fa2046e0dfb9bb67379
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-11-04 12:17:34 -0500

    added function that allows user info and its current time to be saved into local storage

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,14 @@
-// Display today's day and date
+// Display today's day and date on the html page
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
+
+// function that allows the users information and time entered to be stored into local storage
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // Save text in local storage
+        localStorage.setItem(time, text);
+    })
+
